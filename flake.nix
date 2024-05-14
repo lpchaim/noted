d47175f46fd0206e25f8161149e125fa37e8e4b4
Lucas Chaim <lpchaim@gmail.com>
2024-05-14 20:03:25 -0300

    chore: Initial backend commit

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "PHP flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+    in
+    {
+      devShells.default = pkgs.mkShell {
+        packages = with pkgs; [
+          php83
+          php83Packages.composer
+        ];
+      };
+    });
+}
